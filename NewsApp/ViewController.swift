4a5435fd012346914ab1818fc574dfce23b22768
Volha Bychok <olgabychok88@gmail.com>
2020-01-11 12:35:28 +0300

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Ольга Бычок on 11/01/2020.
+//  Copyright © 2020 Ольга Бычок. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
