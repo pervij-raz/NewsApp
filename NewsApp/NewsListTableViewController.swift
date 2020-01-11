6ea32eb06e5fe5958ee177a3737b60a1dfe5b610
Volha Bychok <olgabychok88@gmail.com>
2020-01-11 15:36:57 +0300

    UI is added

---
+++ NewsApp/NewsListTableViewController.swift
@@ -0,0 +1,40 @@
+//
+//  NewsListTableViewController.swift
+//  NewsApp
+//
+//  Created by Ольга Бычок on 11/01/2020.
+//  Copyright © 2020 Ольга Бычок. All rights reserved.
+//
+
+import UIKit
+
+class NewsListTableViewController: UITableViewController {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Table view data source
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+        let newsDetailController = storyBoard.instantiateViewController(withIdentifier: "NewsDetailScreen")
+        self.navigationController?.pushViewController(newsDetailController, animated: true)
+        return nil
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cellID", for: indexPath)
+            as? NewsTableViewCell else {
+                fatalError("Can't find cell")
+        }
+        return cell
+    }
+    
+    
+    
+}
---
+++ NewsApp/NewsTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  NewsTableViewCell.swift
+//  NewsApp
+//
+//  Created by Ольга Бычок on 11/01/2020.
+//  Copyright © 2020 Ольга Бычок. All rights reserved.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var newsImageView: UIImageView!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+}
